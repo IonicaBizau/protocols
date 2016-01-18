dfe3e6e82e9f5bccaf94ba3d01bda9395ef77fd3
Ionică Bizău <bizauionica@gmail.com>
2016-01-18 10:53:50 +0200

    Fix tests

--- test/index.js
+++ test/index.js
@@ -2,24 +2,20 @@ var protocols = require("../lib")
   , tester = require("tester")
   ;
 
-tester.describe("check urls", test => {
-    test.it("should support mutiple protocols", function (cb) {
+tester.describe("check urls", function (test) {
+    test.it("should support mutiple protocols", function () {
         test.expect(protocols("git+ssh://git@some-host.com/and-the-path/name")).toEqual(["git", "ssh"]);
-        cb();
     });
 
-    test.it("should support one protocol", function (cb) {
+    test.it("should support one protocol", function () {
         test.expect(protocols("ssh://git@some-host.com/and-the-path/name")).toEqual(["ssh"]);
-        cb();
     });
 
-    test.it("should support taking the first protocol", function (cb) {
-        test.expect(protocols("git+ssh://git@some-host.com/and-the-path/name").toEqual(true), "git");
-        cb();
+    test.it("should support taking the first protocol", function () {
+        test.expect(protocols("git+ssh://git@some-host.com/and-the-path/name", true)).toBe("git");
     });
 
-    test.it("should support taking the second protocol", function (cb) {
-        test.expect(protocols("git+ssh://git@some-host.com/and-the-path/name").toEqual(1), "ssh");
-        cb();
+    test.it("should support taking the second protocol", function () {
+        test.expect(protocols("git+ssh://git@some-host.com/and-the-path/name", 1)).toBe("ssh");
     });
 });
