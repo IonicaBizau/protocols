4d12b2bc0d2aa01c5338301a97712817754ba2e8
Ionică Bizău <bizauionica@gmail.com>
2016-01-18 10:58:37 +0200

    Fix node version

--- test/index.js
+++ test/index.js
@@ -1,21 +1,21 @@
-var protocols = require("../lib")
-  , tester = require("tester")
-  ;
+const protocols = require("../lib")
+    , tester = require("tester")
+    ;
 
-tester.describe("check urls", function (test) {
-    test.it("should support mutiple protocols", function () {
+tester.describe("check urls", test => {
+    test.it("should support mutiple protocols", () => {
         test.expect(protocols("git+ssh://git@some-host.com/and-the-path/name")).toEqual(["git", "ssh"]);
     });
 
-    test.it("should support one protocol", function () {
+    test.it("should support one protocol", () => {
         test.expect(protocols("ssh://git@some-host.com/and-the-path/name")).toEqual(["ssh"]);
     });
 
-    test.it("should support taking the first protocol", function () {
+    test.it("should support taking the first protocol", () => {
         test.expect(protocols("git+ssh://git@some-host.com/and-the-path/name", true)).toBe("git");
     });
 
-    test.it("should support taking the second protocol", function () {
+    test.it("should support taking the second protocol", () => {
         test.expect(protocols("git+ssh://git@some-host.com/and-the-path/name", 1)).toBe("ssh");
     });
 });
